36eaa11124919491b5f74098cab12d4b3b5d93df
Eduardo del Pozo <eduardo.delpozo@outlook.com>
2019-10-31 08:45:45 +0100

    Changed some things from GitHub

--- MyConsoleAppForMyGitRepository/Program.cs
+++ MyConsoleAppForMyGitRepository/Program.cs
@@ -10,7 +10,7 @@ namespace MyConsoleAppForMyGitRepository
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from GitHub!");
             Console.ReadLine();
         }
     }
